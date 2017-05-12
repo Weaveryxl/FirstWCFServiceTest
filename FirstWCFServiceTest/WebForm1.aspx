29992700faa0f68b57590004eb6d42368e7b468d
Weaveryxl <weaveryxl549@gmail.com>
2017-05-12 13:39:27 -0400

    FirstWCFServiceTest

---
+++ FirstWCFServiceTest/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="FirstWCFServiceTest.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button Text="GetEmployees" ID="btnSubmit" OnClick="btnSubmit_Click" runat="server" /> <br />
+                <asp:Button Text="Divide" ID="btnDivide"  OnClick="btnDivide_Click" runat="server" />
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ FirstWCFServiceTest/WebForm1.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FirstWCFServiceTest.HelloWorldReference;
+
+namespace FirstWCFServiceTest
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private HelloWorldReference.HelloworldServiceClient client = new HelloWorldReference.HelloworldServiceClient();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var result = client.GetEmployee(22);
+            }
+            catch (FaultException<HelloFault> exception)
+            {
+                Response.Write(exception.Detail.ErrorDescription);
+            }
+        }
+
+        protected void btnDivide_Click(object sender, EventArgs e)
+        {
+            //var result = client.GetEmployee(2);
+            try
+            {
+                var res = client.Divide(22, 0);
+            }
+            catch (FaultException<HelloFault> exception)
+            {
+                Response.Write(exception.Detail.ErrorDescription);
+            }
+        }
+    }
+}
